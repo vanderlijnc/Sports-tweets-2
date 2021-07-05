951100b5844887d429129f40708c216b6dbe9a36
sonjakoivisto <37457729+sonjakoivisto@users.noreply.github.com>
2021-07-05 13:14:07 +0300

    Update OLS.r

--- post-processing/OLS.r
+++ post-processing/OLS.r
@@ -1,67 +1,46 @@
-#load required modules (keep the following order)
+#load required packages
 library(foreign)
 library(spatialreg)
 library(spdep)
+library(car)
+library(AICcmodavg)
+library(corrplot)
 
-#importing shapefile dbf file
-data <- read.dbf(file="~/Desktop/YLLI/gradu/gradu/data/PIP_nobots.dbf")
-attach(data)
+#VARIABLE PRESELECTION
+#load data, drop unnecessary columns and give more descriptive column names
+data <- read.dbf(file="~/Desktop/YLLI/gradu/gradu/data/FINAL_PIP.dbf")
+df <- subset(data, select=-c(Posno,Toimip, Toimip_ru, Nimi, Nimi_Ru, Kunta, Kunta_nro, NUMfacilit, Tweets.per, area, Percentage, all_geotag, all_geot_1, X13., Count.of.t, pop, X18.49, X18_49_pop, keskitulo2))
+names(df) <- c("tweets", "median_income", "high_education", "low_income", "high_income", "sport_facilities", "household_size", "low_education", "children", "students", "pensioners", "employed", "unemployed", "own_house", "rental", "kids_house", "adult_house", "pensioner_house")
+#df <- df[ , c(18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)] 
 
+#print out a correlation matrix to pick the variables
+M<-cor(df, use="complete.obs")
+res1 <- cor.mtest(M, conf.level = .95)
+corrplot(M, p.mat = res1$p, method = "color", type= "lower", addCoef.col = "white", order = "hclust", tl.col = "black", tl.srt = 45,number.cex=0.75)
+
+#OLS 
+#run OLS first with all variables and then drop variables according to OLS results and VIF score
+attach(df)
+OLS <- lm(tweets ~ sport_facilities + children + employed + kids_house + adult_house + own_house + rental + household_size)
+vif(OLS)
+AICc(OLS)
+
+#final OLS
+OLS_final <- lm(tweets ~ sport_facilities + children + employed)
+vif(OLS_final)
+AICc(OLS_final)
+
+#LAGRANGE MULTIPLIER TESTS
 #import weights created in GeoDa and convert to R format
 geoda_weights<-read.gal(file="~/Desktop/YLLI/gradu/gradu/data/PIP_nobots.gal",override.id=TRUE)
 weights <- nb2listw(geoda_weights)
 
-#OLS analysis
-OLS <- lm(no_cities_ ~ NUMfacilit + hi_edu + pop + keskitulo2)
-summary(OLS)
-
-#check R squared and p-value
-
-#plot the residuals and check for spatial autocorrelation
-plot(OLS$fitted.values, OLS$residuals)
-moran.test(OLS$residuals, weights)
-moran.plot(OLS$residuals, weights)
-
-#run LM tests to check for better test
-LMtests <- lm.LMtests(OLS, weights, test="all")
+#run LM tests to check if spatial models would be better fit than OLS
+LMtests <- lm.LMtests(OLS_final, weights, test="all")
 print(LMtests)
 
-#which p-value is more significant?
+#no p-value was significant, so I keep the OLS results 
 
-# A) Spatial Error Model
-SEM <- errorsarlm(NUMPOSTS ~ NUMfacilit + hi_income + hi_edu + keskitulo2, data=data, weights)
-sum.SEM <- summary(SEM, Nagelkerke=TRUE)
-print(sum.SEM, signif.stars=TRUE)
-
-#preparation for SDM
-W <- as(as_dgRMatrix_listw(weights), "CsparseMatrix")
-tr <- trW(W, type="MC")
-SDM <- lagsarlm(NUMPOSTS ~ NUMfacilit + hi_income + hi_edu + keskitulo2, data=data, weights, type="mixed", method="MC", trs=tr)
-
-#common factor hypothesis
-CFH <- LR.sarlm(SDM, SEM)
-print(CFH)
-
-#→ if p-value is significant → SDM
-sum.SDM <- summary(SDM, Nagelkerke=TRUE)
-print(sum.SDM, signif.stars=TRUE)
-hist(SDM$residuals)
-plot(SDM$fitted.values, SDM$residuals)
-moran.test(SDM$residuals, weights)
-moran.plot(SDM$residuals, weights)
-
-#to go further, implement direct, indirect and total impacts
-
-
-# B) Spatial Lag Model
-SAR <- lagsarlm(no_cities_ ~ NUMfacilit + hi_edu + pop + mediaanitu, data=data, weights)
-sum.SAR <- summary(SAR, Nagelkerke=TRUE)
-print(sum.SAR, signif.stars=TRUE)
-plot(SAR$fitted.values, SAR$residuals)
-moran.test(SAR$residuals, weights)
-moran.plot(SAR$residuals, weights)
-
-#Residuals should not be spatially autocorrelated
 
 
 
