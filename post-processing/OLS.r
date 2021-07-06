899c348737dd932a7b44135a68d002c35249733e
sonjakoivisto <37457729+sonjakoivisto@users.noreply.github.com>
2021-07-06 14:25:11 +0300

    arrange columns better

--- post-processing/OLS.r
+++ post-processing/OLS.r
@@ -10,8 +10,9 @@ library(corrplot)
 #load data, drop unnecessary columns and give more descriptive column names
 data <- read.dbf(file="~/Desktop/YLLI/gradu/gradu/data/FINAL_PIP.dbf")
 df <- subset(data, select=-c(Posno,Toimip, Toimip_ru, Nimi, Nimi_Ru, Kunta, Kunta_nro, NUMfacilit, Tweets.per, area, Percentage, all_geotag, all_geot_1, X13., Count.of.t, pop, X18.49, X18_49_pop, keskitulo2))
+df <- df[ , c(18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)] 
 names(df) <- c("tweets", "median_income", "high_education", "low_income", "high_income", "sport_facilities", "household_size", "low_education", "children", "students", "pensioners", "employed", "unemployed", "own_house", "rental", "kids_house", "adult_house", "pensioner_house")
-#df <- df[ , c(18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)] 
+
 
 #print out a correlation matrix to pick the variables
 M<-cor(df, use="complete.obs")
